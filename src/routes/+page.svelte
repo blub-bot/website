34670b41b68688da441a3e76d8df0b8e8f619e43
Devin Conway <devinthomas1208@gmail.com>
2022-08-18 06:05:09 -0400

    household

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,13 @@
 <script>
-    import '../app.css'
-    
-    import Desc from '../components/Desc.svelte';
-    import Navbar from '../components/Navbar.svelte';
+	import '../app.css';
+
+	import Desc from '../components/Desc.svelte';
+	import Navbar from '../components/Navbar.svelte';
 </script>
 
 <svelte:head>
-
-    <title>blub. discord bot : coming soon</title>
-
+	<title>blub. discord bot : coming soon</title>
 </svelte:head>
-
 
 <!--Header-->
 <h1 id="header">blub<span style="letter-spacing:0;">.</span></h1>
@@ -21,37 +18,29 @@
 <!--What is blub-->
 <h1 id="what-is">what is blub?</h1>
 
+<!--Description-->
 <Desc />
 
+<!--"Navbar"-->
 <Navbar />
 
 <style>
+	#header {
+		top: 0;
+		margin: 0;
 
+		font-size: clamp(5rem, 13vw, 16rem);
+		line-height: 265px;
+		letter-spacing: clamp(3rem, 15vw, 20rem);
+	}
 
-    #header {
+	#coming-soon {
+		font-size: clamp(3rem, 7vw, 8rem);
+		margin: 0;
+	}
 
-        top: 0;
-        margin: 0;
-        
-        font-size: clamp(5rem, 13vw, 16rem);
-        line-height: 265px;
-        letter-spacing: clamp(3rem, 15vw, 20rem);
-
-    }
-
-    #coming-soon {
-
-        font-size: clamp(3rem, 7vw, 8rem);
-        margin: 0;
-
-    }
-
-    #what-is {
-
-        font-size: clamp(2rem, 5vw, 4rem);
-        text-decoration: underline;
-
-    }
-
-
+	#what-is {
+		font-size: clamp(2rem, 5vw, 4rem);
+		text-decoration: underline;
+	}
 </style>
